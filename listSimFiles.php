1e410025c72a90527d08a4c0f781797a9446a16a
Cosmin Toader <cosmin.toader.nicu@gmail.com>
2017-10-06 10:19:49 +0200

    Added simulation file picker.

---
+++ listSimFiles.php
@@ -0,0 +1,33 @@
+<?php
+$simFolderPath = './content/Simulations/';
+$simFolderContent = scandir($simFolderPath);
+$simFoldersToCheck = [];
+$stdFiles = ["_SimulationFiles.json.scf"
+        , "LocalNeuronCommonInfo.json"
+        , "LocalNeuronInfo_0.json"
+        , "GlobalSimulationParamsV2.json"
+        , "GlobalParams_0.json"
+        , "Connectivity_0.json"];
+
+foreach ($simFolderContent as $elem) {
+  if ($elem != '..' && $elem != '.' && is_dir($simFolderPath . $elem)) {
+    array_push($simFoldersToCheck, $elem);
+  }
+}
+
+$validSimFolders = [];
+foreach ($simFoldersToCheck as $folder) {
+	$folderContent = scandir($simFolderPath . $folder);
+	$isVaildContet = true;
+	foreach ($stdFiles as $file) {
+		if (!in_array($file, $folderContent)) {
+			$isVaildContet = false;
+    			break;
+		}
+	}
+	if($isVaildContet) {
+		array_push($validSimFolders, $folder);
+	}
+}
+echo json_encode($validSimFolders);
+?>
